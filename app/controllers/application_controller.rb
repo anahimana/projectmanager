57f5c17678ba8847aff2d2e22991de3198564775
anahimana <alex.nahimana1@gmail.com>
2020-01-26 12:00:57 -0600

    Enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "#{ENV['SESSION_SECRET']}"
   end
+
 end
