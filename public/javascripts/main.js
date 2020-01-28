1f2d695aceefe65fc7dfa2759d47afe3097125d7
anahimana <alex.nahimana1@gmail.com>
2020-01-28 08:19:36 -0600

    Add button event listener

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,9 @@
 // Custom JS goes here
+
+// GLOBALS
+const deleteButton = document.getElementById('btn-dlt')
+
+// EVENT LISTENERS
+deleteButton.addEventListener('click', function (event) {
+  console.log(`${event.target.innerText} has been clicked!`);
+});
