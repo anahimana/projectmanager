2b6afac51ffcc5cf06e63bf8c6805249d53e834d
anahimana <alex.nahimana1@gmail.com>
2020-01-28 11:50:21 -0600

    Add User#full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, on: :create
   has_many :projects
   has_secure_password
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
